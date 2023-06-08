e34fcb0267b1316bad456dc307c68cdd3e6b6f2f
Kokoteros <marcosjose.piedra@gmail.com>
2023-06-08 20:03:19 -0300

    Initial commit

---
+++ src/main/java/org/fut5app/info/App.java
@@ -0,0 +1,13 @@
+package org.fut5app.info;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
